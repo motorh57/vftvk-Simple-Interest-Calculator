d433c992a4c34dfbeb5422684bceffcbec60277b
MacBook White <MacBookWhite@MacBook-Whites-MacBook.local>
2021-07-21 15:11:18 -0500

    added a new calculator working-index.html

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //Retriving and store the variables to calculate
+	var principal= document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+	//Making the Calculation
+    var interest = principal*years*rate/100;
+    var year = new Date().getFullYear()+parseInt(years);
+	//Printing result to hogligth the results we use the mark tag inside the html
+    var results = document.getElementById("result").innerHTML= 
+    "If you deposit <mark>  "+principal+ " </mark>,<br/> at an interest rate of <mark> "
+    + rate+" </mark> %,</br> You Will recibe an amount of : <mark> " +interest+ 
+    " </mark> ,</br> in the year: <mark> "+year;" </mark> <br>";
     
 }
-        
+        //Funtions to show the amount of the selection at displacement bar
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value; 
+    document.getElementById("rate_val").innerText=rateval;
+}
+function checkvalues ()
+{
+    var investment=document.getElementById("principal");
+    if (investment.value==""){
+        alert("the field Amount Can not be empty");
+ 		investment.focus();
+        return false;
+    }
+} 
+function bigerThan ()
+{
+	var invAmount=document.getElementById("principal");
+	if(invAmount.value<"1"){
+		alert("!Error! Enter a Positive Number");
+		invAmount.focus();
+		return false;
+	}
+}
